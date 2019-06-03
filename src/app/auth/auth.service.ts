62486ee5010b3e2712225c9fd67b513ef31b17f1
Luis Miguel Del Gallego Horta <luis.delgallego@avaldigitallabs.com>
2019-06-02 21:32:49 -0500

    Implement logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import * as firebase from 'firebase';
 
 import Swal from 'sweetalert2';
 
@@ -13,11 +14,16 @@ export class AuthService {
   constructor( private afAuth: AngularFireAuth,
     private router: Router ) { }
 
+  initAuthListener() {
+    this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
+      console.log(fbUser);
+    });
+  }
+
   crearUsuario( nombre: string, email: string, password: string) {
     this.afAuth.auth
     .createUserWithEmailAndPassword(email, password)
     .then( resp => {
-      console.log(resp);
       this.router.navigate(['/']);
     })
     .catch( error => {
@@ -30,13 +36,18 @@ export class AuthService {
     this.afAuth.auth
     .signInWithEmailAndPassword(email, password)
     .then( resp => {
-      console.log(resp);
       this.router.navigate(['/']);
     })
     .catch( error => {
-      console.error(error);
       Swal.fire('Error en el login', error.message, 'error');
     });
+  }
+
+  logOut() {
+    this.afAuth.auth.signOut()
+    .then( resp => {
+      this.router.navigate(['/login']);
+    } );
   }
 
 }
